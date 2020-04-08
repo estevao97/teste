28b3ebc01353033224362fe9aace6f57ffe3dfed
Paulo Estêvão <pauloestevao92@gmail.com>
2020-04-08 01:52:59 -0300

    Apenas um teste

--- src/application/Proogram.java
+++ src/application/Proogram.java
@@ -1,9 +1,19 @@
 package application;
 
+import java.util.Locale;
+
 public class Proogram {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		double x = 10.35784;
+		System.out.println("Olá corno!%nMuuuuuu!");
+		Locale.setDefault(Locale.US);
+		System.out.println(x);
+		System.out.printf("%.2f%n", x);
+		System.out.printf("%.4f%n", x);
+		System.out.println("RESULTADO = " + x + " metros");
+
 
 	}
 
